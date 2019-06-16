9c0c29a86694c51b5ba1b8d3eff494e52adc976b
Dane Petersen <thegreatsunra@gmail.com>
2019-06-15 21:45:37 -0500

    Tweak toolbar and move into its own component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,6 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
+    <app-toolbar />
     <v-content>
       <hello-world />
     </v-content>
@@ -23,11 +9,13 @@
 
 <script>
 import HelloWorld from '@/components/HelloWorld'
+import AppToolbar from '@/components/AppToolbar'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    AppToolbar,
   },
   data () {
     return {
---
+++ src/components/AppToolbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-toolbar
+    app
+    clipped-left
+    fixed
+    dark
+    color="black"
+  >
+    <v-toolbar-side-icon
+    >
+    </v-toolbar-side-icon>
+    <v-toolbar-title>Vuetify Seed</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn icon>
+      <v-icon>person</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+</script>
