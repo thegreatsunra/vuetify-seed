af29456c57df61723d73b700b153bc6f305d9622
Dane Petersen <thegreatsunra@gmail.com>
2019-08-15 20:49:49 -0500

    Improve router and import hello-world component with different syntax

--- src/router.js
+++ src/router.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from '@/views/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
-      component: Home
+      component: Home,
+      alias: '/'
     },
     {
       path: '/about',
@@ -17,7 +18,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
     }
   ]
 })
