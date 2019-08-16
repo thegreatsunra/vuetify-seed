e1070ff084979ccb0d7db48fa797ac367ee7722c
Dane Petersen <thegreatsunra@gmail.com>
2019-08-15 20:50:16 -0500

    Update vuetify to 2.0

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
@@ -12,5 +12,6 @@ Vue.prototype.$eventBus = new Vue()
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
